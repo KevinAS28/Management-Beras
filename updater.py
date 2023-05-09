9970b333373d2fe878f718f3bf001dda8496e888
Kevin AS <40710024+KevinAS28@users.noreply.github.com>
2023-05-09 21:30:57 +0700

    update updater

--- updater.py
+++ updater.py
@@ -9,7 +9,7 @@ with open(os.path.join(str(Path.home()), 'beras.dat'), 'r') as dat_file:
     git_exe = os.path.join(current_dir, 'portablegit', 'cmd', 'git.exe')
 
 commands = [
-    f'call "{git_exe}: add .',
+    f'call "{git_exe}" add .',
     f'call "{git_exe}" stash',
     f'call "{git_exe}" pull origin main',
 ]
