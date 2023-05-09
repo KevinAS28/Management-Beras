ce442cca50146ae703bda86ff3a61b71765f8288
Kevin AS <40710024+KevinAS28@users.noreply.github.com>
2023-05-09 21:30:15 +0700

    update updater

--- updater.py
+++ updater.py
@@ -6,10 +6,13 @@ with open(os.path.join(str(Path.home()), 'beras.dat'), 'r') as dat_file:
     dat = json.loads(dat_file.read())
     current_dir = dat['project_path']
     python_exe = os.path.join(current_dir, 'python.exe')
-    git_exe = os.path.join(current_dir, 'cmd', 'git.exe')
+    git_exe = os.path.join(current_dir, 'portablegit', 'cmd', 'git.exe')
 
 commands = [
-    f'{git_exe} add .',
-    f'{git_exe} stash',
-    f'{git_exe} pull origin main',
+    f'call "{git_exe}: add .',
+    f'call "{git_exe}" stash',
+    f'call "{git_exe}" pull origin main',
 ]
+
+for com in commands:
+    os.system(com)
