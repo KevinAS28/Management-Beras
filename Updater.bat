0430f95a278fa6d3f2dd1c86f25a43e27eadc58c
Kevin AS <40710024+KevinAS28@users.noreply.github.com>
2023-05-09 21:09:53 +0700

    update updater

--- Updater.bat
+++ Updater.bat
@@ -1,6 +1,6 @@
 set gitdir=C:\Users\kevin\Documents\beraspakde\portablegit
 set path=%gitdir%\cmd;%path%
-git add beraspakde cost_calculator static templates create_shortcut.py get-pip.py install_run.py manage.py shell_module.py
+git add .
 git stash
 git pull origin main
 pause
